931062bf5eca72c09ddb8ad13482f650c089c584
Andrew Miller <206alm@gmail.com>
2019-09-22 09:40:36 -0700

    added some external CSS styling, ready to create JS game

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//QUESTION AND ANSWER VARIABLES
+var questionOne = "Which pro golfer is depicted in this image?";
+var ansArrOne = ["Jack Nicklaus", "Brad Faxon", "John Daly", "Tiger Woods"];
+var questionTwo = "Which pro golfer currently holds the #1 World Golf Ranking?";
+var ansArrTwo = ["Justin Rose", "Rory McIlroy", "Dustin Johnson", "Brooks Kopeka"];
+var questionThree = "In which country was the game of golf formally invented?"
+var ansArrThree = ["England", "Scotland", "Ireland", "America"];
+var questionFour = "How many players are there on the PGA tour?";
+var ansArrFour = ["125", "250", "245", "200"];
+var questionFive = "Which pro golfer holds the most major titles?";
+var ansArrFive = ["Arnold Palmer", "Bobby Jones", "Byron Nelson", "Jack Nicklaus"];
+//GAMEPLAY VARIABLES
+var ansCorrect = 0;
+var ansIncorrect = 0;
+var wins = 0;
+var losses = 0;
